c791bdf159048e35ad83e50d5f0610003eae10c1
IhorMar <rmg1203@gmail.com>
2022-08-04 10:08:50 +0300

    Created files for setting Admin Pannel

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.utils.safestring import mark_safe
+
 from women.models import Women, Category
 
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('time_create', 'is_published')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo','is_published','time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update','get_html_photo')
 
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Miniatura'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -22,3 +31,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Admin-pannel about women'
+admin.site.site_header = 'Admin-pannel about women-@@@'
